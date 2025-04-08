5df33e6ccd36a80d84eee11daa566df191dfccf7
jazielSlayer <jazielarmandovargaschoque.com>
2025-04-08 19:18:20 -0400

    Actualizacion de PORT en index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from './app';
 import './database';
 
-const port = process.env.PORT || 3000;
+const port = process.env.DB_PORT || 3000;
 app.listen(port);
 console.log('Server on port', port);
